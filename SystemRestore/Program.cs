4b4f64a7af69a358da0333c9374b8265fe4c606a
ebrunton <edbrunton@email.arizona.edu>
2022-03-13 20:40:14 -0500

    Document and cleanup

--- SystemRestore/Program.cs
+++ SystemRestore/Program.cs
@@ -8,18 +8,49 @@ string equiv = @"C:\<YourTargetDirectory";
 CopyFile(root, equiv);
 Console.WriteLine("Done");
 
-
-
+/// <summary>
+/// Copies the files from the folder to the target folder
+/// </summary>
 void CopyFile(string folder, string targetFolder)
 {
     foreach (string nextFolder in Directory.GetDirectories(folder))
     {
-        string nextPiece = nextFolder.Substring(folder.Length);
+        string nextPiece = nextFolder[folder.Length..];
         string newFolder = targetFolder + nextPiece;
         CopyFile(nextFolder, newFolder);
     }
-    var allFiles = Directory.GetFiles(folder);
-    Dictionary<string, FileInfo> coveredFiles = new Dictionary<string, FileInfo>();
+    string[] allFiles = Directory.GetFiles(folder);
+    Dictionary<string, FileInfo> coveredFiles = MostRecentFiles(allFiles);
+    if (!Directory.Exists(targetFolder))
+    {
+        Directory.CreateDirectory(targetFolder);
+    }
+    foreach (KeyValuePair<string, FileInfo> keyVal in coveredFiles)
+    {
+        string newFileName = targetFolder + '\\' + keyVal.Key;
+        if (File.Exists(newFileName))
+        {
+            continue;//don't overwrite existing files
+        }
+        string currentFile = keyVal.Value.FullPath;
+        try
+        {
+            File.Copy(currentFile, newFileName, false);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(string.Format("Could not copy {0} to {1}: {2}", currentFile, newFileName, ex));
+        }
+    }
+
+
+}
+/// <summary>
+/// Gets the location of the most recent files keys by the file name (with no file path
+/// </summary>
+Dictionary<string, FileInfo> MostRecentFiles(string[] allFiles)
+{
+    Dictionary<string, FileInfo> coveredFiles = new();
     foreach (string file in allFiles)
     {
         if (file.Contains("UTC)"))
@@ -33,7 +64,7 @@ void CopyFile(string folder, string targetFolder)
             {
                 coveredFiles.Add(fileName, new FileInfo()
                 {
-                    FullName = file,
+                    FullPath = file,
                     Date = dateTime,
                 });
             }
@@ -41,7 +72,7 @@ void CopyFile(string folder, string targetFolder)
             else if (coveredFiles[fileName].Date < dateTime)
             {
                 coveredFiles[fileName].Date = dateTime;
-                coveredFiles[fileName].FullName = file;
+                coveredFiles[fileName].FullPath = file;
             }
         }
         else
@@ -50,122 +81,108 @@ void CopyFile(string folder, string targetFolder)
         }
     }
 
-    if (!Directory.Exists(targetFolder))
-    {
-        Directory.CreateDirectory(targetFolder);
-    }
-    foreach (var keyVal in coveredFiles)
-    {
-        string newFileName = targetFolder + '\\' + keyVal.Key;
-        if (!File.Exists(newFileName))
-        {
-            string currentFile = keyVal.Value.FullName;
-            try
-            {
-                File.Copy(currentFile, newFileName, false);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(string.Format("Could not copy {0} to {1}: {2}", currentFile, newFileName, ex));
-            }
-        }
-    }
+    return coveredFiles;
 }
-
 /// <summary>
 /// Removes the UTC extension
 /// </summary>
 string GetFixedPath(string path, out DateTime dateTime)
 {
     dateTime = default;
-    if (path.Contains("UTC)"))
+    if (!path.Contains("UTC)"))
     {
-        List<char> sb = new(path.Length);
-        bool skipBegin = false;
-        bool skipEnd = false;
-        string year = "";
-        string month = "";
-        string day = "";
-        string hour = "";
-        string minute = "";
-        string second = "";
-        for (int i = path.Length - 1; i >= 0; i--)
+        return path;
+    }
+    List<char> sb = new(path.Length);
+    bool skipBegin = false;
+    bool skipEnd = false;
+    string year = "";
+    string month = "";
+    string day = "";
+    string hour = "";
+    string minute = "";
+    string second = "";
+    for (int i = path.Length - 1; i >= 0; i--)
+    {
+        char currentChar = path[i];
+        if (currentChar == '\\')
         {
-            char currentChar = path[i];
-            if (currentChar == '\\')
+            break;
+        }
+        if (skipEnd)
+        {
+            sb.Add(currentChar);
+        }
+        else if (skipBegin && currentChar == '(')
+        {
+            skipEnd = true;
+            i--;
+        }
+        else if (currentChar == ')')
+        {
+            skipBegin = true;
+        }
+        else if (!skipBegin)
+        {
+            sb.Add(currentChar);
+        }
+        else if (skipBegin)
+        {
+            if (!char.IsDigit(currentChar))
             {
-                break;
-            }
-            if (skipEnd)
-            {
-                sb.Add(currentChar);
-            }
-            else if (skipBegin && currentChar == '(')
-            {
-                skipEnd = true;
-                i--;
-            }
-            else if (currentChar == ')')
-            {
-                skipBegin = true;
-            }
-            else if (!skipBegin)
-            {
-                sb.Add(currentChar);
-            }
-            else if (skipBegin)
-            {
-                if (!char.IsDigit(currentChar))
-                {
 
-                }
-                else if (second.Length != 2)
+            }
+            else if (second.Length != 2)
+            {
+                second = currentChar + second;
+            }
+            else if (minute.Length != 2)
+            {
+                minute = currentChar + minute;
+            }
+            else if (hour.Length != 2)
+            {
+                hour = currentChar + hour;
+            }
+            else if (day.Length != 2)
+            {
+                day = currentChar + day;
+            }
+            else if (month.Length != 2)
+            {
+                month = currentChar + month;
+            }
+            else if (year.Length != 4)
+            {
+                year = currentChar + year;
+                if (year.Length == 4)
                 {
-                    second = currentChar + second;
-                }
-                else if (minute.Length != 2)
-                {
-                    minute = currentChar + minute;
-                }
-                else if (hour.Length != 2)
-                {
-                    hour = currentChar + hour;
-                }
-                else if (day.Length != 2)
-                {
-                    day = currentChar + day;
-                }
-                else if (month.Length != 2)
-                {
-                    month = currentChar + month;
-                }
-                else if (year.Length != 4)
-                {
-                    year = currentChar + year;
-                    if (year.Length == 4)
+                    try
                     {
-                        try
-                        {
-                            dateTime = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day), int.Parse(hour), int.Parse(minute), int.Parse(second));
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(string.Format("Failed to interpret {0}: {1}", path, e));
-                        }
+                        dateTime = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day), int.Parse(hour), int.Parse(minute), int.Parse(second));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(string.Format("Failed to interpret {0}: {1}", path, e));
                     }
                 }
             }
         }
-        return new string(sb.AsEnumerable().Reverse().ToArray());
     }
-    return path;
+    return new string(sb.AsEnumerable().Reverse().ToArray());
 }
 /// <summary>
 /// Helper class
 /// </summary>
 class FileInfo
 {
+    /// <summary>
+    /// The UTC date of the file's backup time
+    /// </summary>
     public DateTime Date { get; set; }
-    public string FullName { get; set; } = "";
+    /// <summary>
+    /// The full file path with the file name
+    /// </summary>
+    public string FullPath { get; set; } = "";
 }
 
